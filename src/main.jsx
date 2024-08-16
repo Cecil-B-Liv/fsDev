2a3b155e4466688bd07cd87aeaca7bd0c1f4f479
Milkyway Galaxy <blannkisblank@gmail.com>
2024-08-16 22:30:57 +0700

    Update structure
    holy fucking shit bullshit ahh

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginSignUpPage from "./pages/LoginSignUpPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginSignUpPage />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
