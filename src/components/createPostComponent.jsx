1a44230ccbfb761f8c7d4b7283aba8591f163c57
Milkyway Galaxy <blannkisblank@gmail.com>
2024-08-19 15:10:48 +0700

    Update createPostComponent.jsx

--- src/components/createPostComponent.jsx
+++ src/components/createPostComponent.jsx
@@ -3,13 +3,11 @@ export default function CreatePostComponent() {
         <>
             <div className="create-post-container">
                 <div className="post-header">
-                    <a>Create Post</a>
+                    <a className="avatar">avatar</a>
                     <button className="cancel-btn">X</button>
                 </div>
-
-                <div className="line2"></div>
                 <textarea style={{ resize: "none" }} className="user-input" placeholder="what you thinkin' blud"></textarea>
-                <button className='new-account-btn'>Create new account</button>
+                <button className='post-btn'>Post</button>
             </div >
         </>
     )
