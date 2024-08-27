1ad1ad3ef528812f71a9068ea333b201e7a9f02f
Frank-Vu <s3963172@rmit.edu.vn>
2024-08-27 20:03:05 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ import { fileURLToPath } from 'url';
 
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
+import { createPost } from './controllers/posts.js';
+import { verifyToken } from './controllers/auth.js';
 
 import cookieparser from 'cookie-parser';
 
@@ -43,10 +46,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILE UPLOAD*/
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGODB CONNECTION */
 const port = process.env.PORT || 6001;
