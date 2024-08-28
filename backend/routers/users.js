6377e5e80ae5ea297583720dd66288d3e0b19edd
Frank-Vu <s3963172@rmit.edu.vn>
2024-08-28 16:25:42 +0700

    Redo users.js in controller and router

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,30 +1,42 @@
 import express from "express";
 import {
     getUser,
-    // Friend
-    getUserFriendList,
-    addRemoveFriend,
-    getFriendRequests,
+    updateUserProfile,
+    getUserFriends,
+    removeFriend,
+    sendFriendRequest,
+    sendGroupJoinRequest,
     acceptFriendRequest,
-    denyFriendRequest,
-    // Group
-    getUserGroupList,
-    addUserGroup,
+    declineFriendRequest,
+    searchUsers,
+    suspendUser,
+    resumeUser,
+    getUserGroups,
 } from "../controllers/users.js";
-import { verifyToken } from "../middleware/auth.js";
+import { isAuthenticated, isSiteAdmin } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+/* CREATE */
+router.post('/users/:userId/friendRequests', isAuthenticated, sendFriendRequest); // Send a friend request
+router.post('/users/:userId/groupRequests', isAuthenticated, sendGroupJoinRequest);   // Send a group request
+
 /* READ */
-router.get("/:id", verifyToken, getUser);
-router.get("/:id/friends", verifyToken, getUserFriendList);
-router.get("/:id/groups", verifyToken, getUserGroupList);
-router.get("/:id/friendRequests", verifyToken, getFriendRequests);
+router.get("/users/:userId", isAuthenticated, getUser);   // Get user profile
+router.get("/users/:userId/friends", isAuthenticated, getUserFriends);    // Get user's friends
+router.get("/users/:userId/groups", isAuthenticated, getUserGroups);  // Get user's groups
+router.get('/search', isAuthenticated, searchUsers);  // Search user
 
 /* UPDATE */
-router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
-router.patch("/:id/:groupId", verifyToken, addUserGroup);
-router.patch("/:id/acceptFriendRequest/:friendId", verifyToken, acceptFriendRequest);
-router.patch("/:id/denyFriendRequest/:friendId", verifyToken, denyFriendRequest);
+router.patch("/users/:userId", isAuthenticated, updateUserProfile);
+router.patch('/friendRequests/:requestId/accept', isAuthenticated, acceptFriendRequest);  // Accept a friend request
+router.patch('/friendRequests/:requestId/decline', isAuthenticated, declineFriendRequest);    // Decline a friend request
+
+/* DELETE */
+router.delete("/users/:userId/friends/:friendId", isAuthenticated, removeFriend); // Remove a friend
+
+/* Suspend or Resume user (admin only) */
+router.put("/users/:userId/suspend", isAuthenticated, isSiteAdmin, suspendUser);
+router.put("/users/:userId/resume", isAuthenticated, isSiteAdmin, resumeUser);
 
 export default router;
