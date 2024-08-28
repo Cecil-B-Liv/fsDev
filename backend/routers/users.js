706a6d82e79de140a673770a16ae9eb831bffb58
Frank-Vu <s3963172@rmit.edu.vn>
2024-08-28 17:01:53 +0700

    Update users.js in routers

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -19,17 +19,17 @@ import upload from "../middlewares/upload.js";
 const router = express.Router();
 
 /* CREATE */
-router.post('/users/:userId/friendRequests', isAuthenticated, sendFriendRequest); // Send a friend request
-router.post('/users/:userId/groupRequests', isAuthenticated, sendGroupJoinRequest);   // Send a group request
+router.post('/:userId/friendRequests', isAuthenticated, sendFriendRequest); // Send a friend request
+router.post('/:userId/groupRequests', isAuthenticated, sendGroupJoinRequest);   // Send a group request
 
 /* READ */
-router.get("/users/:userId", isAuthenticated, getUser);   // Get user profile
-router.get("/users/:userId/friends", isAuthenticated, getUserFriends);    // Get user's friends
-router.get("/users/:userId/groups", isAuthenticated, getUserGroups);  // Get user's groups
+router.get("/:userId", isAuthenticated, getUser);   // Get user profile
+router.get("/:userId/friends", isAuthenticated, getUserFriends);    // Get user's friends
+router.get("/:userId/groups", isAuthenticated, getUserGroups);  // Get user's groups
 router.get('/search', isAuthenticated, searchUsers);  // Search user
 
 /* UPDATE */
-router.patch("/users/:userId",
+router.patch("/:userId",
     isAuthenticated,
     upload.single("picture"),   // add upload middleware
     updateUserProfile);
@@ -37,10 +37,10 @@ router.patch('/friendRequests/:requestId/accept', isAuthenticated, acceptFriendR
 router.patch('/friendRequests/:requestId/decline', isAuthenticated, declineFriendRequest);    // Decline a friend request
 
 /* DELETE */
-router.delete("/users/:userId/friends/:friendId", isAuthenticated, removeFriend); // Remove a friend
+router.delete("/:userId/friends/:friendId", isAuthenticated, removeFriend); // Remove a friend
 
 /* Suspend or Resume user (admin only) */
-router.put("/users/:userId/suspend", isAuthenticated, isSiteAdmin, suspendUser);
-router.put("/users/:userId/resume", isAuthenticated, isSiteAdmin, resumeUser);
+router.put("/:userId/suspend", isAuthenticated, isSiteAdmin, suspendUser);
+router.put("/:userId/resume", isAuthenticated, isSiteAdmin, resumeUser);
 
 export default router;
