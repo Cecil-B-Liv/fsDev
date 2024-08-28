2cf15ea96a172372e94bb1f6861f67f247adcc51
Frank-Vu <s3963172@rmit.edu.vn>
2024-08-28 19:38:11 +0700

    Add userBio and rep to comment

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
 
-    // add id adminOfGrp[]
-    // add string bio 
+    /* 
+    add id adminOfGrp[] (the groupAdmin id will be stored in the group model so if the user is the admin of that group, that group will store that user id in its property call groupAdminId)
+    */
     {
         username:
         {
@@ -31,6 +32,11 @@ const UserSchema = new mongoose.Schema(
             type: String,
             required: true,
             min: 5,
+        },
+        userBio:
+        {
+            type: String,
+            max: 200,
         },
         friendList:
             [{
