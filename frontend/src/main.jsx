d9ffd9fa7427f27ed2b4682cd5c35d8470065e5a
Frank-Vu <s3963172@rmit.edu.vn>
2024-09-06 11:52:51 +0700

    Update main.jsx for development

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,169 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LoginSignupPage from "./pages/LoginSignupPage";
-import HomePage from "./pages/Homepage";
-import Feed from "./components/feedComponent";
-import NotFound from "./pages/NotFound";
-import UserGroupsComponent from "./components/userGroupsComponent";
-import FriendList from "./components/friendListComponent";
-import CreateGroupComponent from "./components/createGroupComponent";
-import GroupWall from "./components/groupWallComponent";
-import FriendRequest from "./components/friendRequestCardComponent";
-import SiteAdmin from "./pages/SiteAdminpage";
-import GroupCreateRequestList from "./components/groupCreateRequestList";
-import GroupAdminPage from "./pages/GroupAdminPage";
-import JoinRequestList from "./components/joinRequestList";
-import MemberManageList from "./components/memberManageList";
-import Profile from "./components/ProfileComponent"
-import ManageUsersList from "./components/manageUserList";
+// import LoginSignupPage from "./pages/LoginSignupPage";
+// import HomePage from "./pages/Homepage";
+// import Feed from "./components/feedComponent";
+// import NotFound from "./pages/NotFound";
+// import UserGroupsComponent from "./components/userGroupsComponent";
+// import FriendList from "./components/friendListComponent";
+// import CreateGroupComponent from "./components/createGroupComponent";
+// import GroupWall from "./components/groupWallComponent";
+// import FriendRequest from "./components/friendRequestCardComponent";
+// import SiteAdmin from "./pages/SiteAdminpage";
+// import GroupCreateRequestList from "./components/groupCreateRequestList";
+// import GroupAdminPage from "./pages/GroupAdminPage";
+// import JoinRequestList from "./components/joinRequestList";
+// import MemberManageList from "./components/memberManageList";
+// import Profile from "./components/ProfileComponent"
+// import ManageUsersList from "./components/manageUserList";
 
-// Data test add by NA
-const groups = [
-  {
-    groupId: 1,
-    groupName: "Group 1",
-    groupDescription: "Demo Group, This group link is active",
-  },
-  {
-    groupId: 2,
-    groupName: "Group 2",
-    groupDescription: "Description 2",
-  },
-  {
-    groupId: 3,
-    groupName: "Group 1",
-    groupDescription: "Description 1",
-  },
-  {
-    groupId: 4,
-    groupName: "Group 2",
-    groupDescription: "Description 2",
-  },
-  {
-    groupId: 5,
-    groupName: "Group 1",
-    groupDescription: "Description 1",
-  },
-  {
-    groupId: 6,
-    groupName: "Group 2",
-    groupDescription: "Description 2",
-  },
-];
+// REMOVED BY TUNG
+// ALREADY SAVED THE OLD CODE. ASK IF NEED
 
-
+/* ROUTER */
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LoginSignupPage />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/HomePage",
-    element: <HomePage />,
-    // used to protect the homepage when isnt logged in
-    // element: (
-    //   <ProtectedRoute>  {/* Wrap the protected HomePage route */}
-    //     <HomePage />
-    //   </ProtectedRoute>
-    // ),
-    children: [
-      {
-        index: true,
-        element: <Feed />,
-      },
-      {
-        path: "Groups",
-        element: <UserGroupsComponent groups={groups} />,
-      },
-      {
-        path: "Profile",
-        element: <Profile/>,
-      },
-      {
-        path: "Groups/:GroupID",
 
-        element: <GroupWall />,
-        children: [
-          {
-            index: true,
-            element: <Feed/>
-          },
-          {
-            path: "GroupFeeds",
-            element: <Feed />,
-          },
-          {
-            path: "GroupMemebers",
-            element: <FriendList />,
-          },
-        ],
-      },
-      {
-        path: "Friends",
-        element: <FriendList />,
-      },
-      {
-        path: "CreateGroup",
-        element: <CreateGroupComponent />,
-      },
-      {
-        path: "FriendRequests",
-        element: <FriendRequest/>,
-      },
-    ],
-  },
-  {
-    path: "/HomePage/Groups/:GroupID/ManageGroup",
-    element: <GroupAdminPage/>,
-    children: [
-      {
-        index:true,
-        element: <JoinRequestList/>
-      },
-      {
-        path: "JoinRequests",
-        element: <JoinRequestList/>
-      },
-      {
-        path:"Members",
-        element: <MemberManageList/>
-      }
-    ]
-  },
 
-  // Site Admin
-  {
-    path: "SiteAdmin",
-    element: <SiteAdmin/>,
-    children:[
-      {
-        index: true,
-        element: <GroupCreateRequestList/>
-      },
-      {
-        path: "Requests",
-        element: <GroupCreateRequestList/>
-      },
-      {
-        path: "Users",
-        element: <ManageUsersList/>
-      },
-      {
-        path: "Feed",
-        element: <Feed/>
-      }
-    ]
-  },
- 
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
