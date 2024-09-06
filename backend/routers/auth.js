708e8711cb54388a480d637be5ca21afab706410
Frank-Vu <s3963172@rmit.edu.vn>
2024-09-07 05:57:57 +0700

    Update auth.js router

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.js";
+import {
+    register,
+    login,
+    checkAuth,
+    logout
+} from "../controllers/auth.js";
 import {
     validateLogin,
-    validateRegister
+    validateRegistration
 } from "../middlewares/validate.js"
 import { uploadUserPicture } from "../middlewares/upload.js";
 
@@ -11,16 +16,20 @@ const router = express.Router();
 // Handle user registration with picture upload
 router.post("/register",
     uploadUserPicture,      // Add upload middleware
-    validateRegister,       // Add validation middleware
+    validateRegistration,       // Add validation middleware
     register
 );
 
 // Handle user login
 router.post("/login",
     validateLogin,      // Add validation middleware
-    login);
+    login
+);
+
+// Handle user authentication status check
+router.get("/check-auth", checkAuth);
 
 // Handle user logout
-router.post("/logout", logout);
+router.delete("/logout", logout);
 
 export default router;
