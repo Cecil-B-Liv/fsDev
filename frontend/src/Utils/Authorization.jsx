01a6852ac3adbc20838650e6f57847c2caa61df7
Frank-Vu <s3963172@rmit.edu.vn>
2024-09-08 06:55:30 +0700

    Update authorization

--- frontend/src/Utils/Authorization.jsx
+++ frontend/src/Utils/Authorization.jsx
@@ -1,21 +1,48 @@
 // Need to redo again (Tung)
 
-import { useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { Navigate, useLocation } from 'react-router-dom';
 
-const useAuthorization = (allowedRoles) => {
-  const { user, isAuthenticated } = useSelector((state) => state.auth);
-  const location = useLocation();
+// const useAuthorization = (allowedRoles) => {
+//   const { user, isAuthenticated } = useSelector((state) => state.auth);
+//   const location = useLocation();
 
-  // Check if the user is authenticated and has an allowed role
-  const isAuthorized = isAuthenticated && allowedRoles.includes(user?.userRole);
+//   // Check if the user is authenticated and has an allowed role
+//   const isAuthorized = isAuthenticated && allowedRoles.includes(user?.userRole);
 
-  if (!isAuthorized) {
-    // If not authorized, navigate to the forbidden page or another suitable location
-    return <Navigate to="/forbidden" state={{ from: location }} replace />;
+//   if (!isAuthorized) {
+//     // If not authorized, navigate to the forbidden page or another suitable location
+//     return <Navigate to="/forbidden" state={{ from: location }} replace />;
+//   }
+
+//   return null; // Return null if authorized, allowing the component to render normally
+// };
+
+// export default useAuthorization;
+
+import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import { checkAuth } from '../apis/auth.js';
+
+const useAuthorization = () => {
+  const [userRole, setUserRole] = useState("");
+
+  useEffect(() => {
+    const checkRole = async () => {
+      const response = await checkAuth();
+      setUserRole(response.userRole);
+    };
+    checkRole();
+  }, []);
+
+  if (userRole == "groupAdmin") {
+    return <Outlet />;
   }
 
-  return null; // Return null if authorized, allowing the component to render normally
-};
+  if (userRole == "siteAdmin") {
+    return <Outlet />;
+  }
+}
 
 export default useAuthorization;
