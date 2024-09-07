995b61caef6f5c235bf319a4d2e282c7e32e740f
Frank-Vu <s3963172@rmit.edu.vn>
2024-09-07 18:22:53 +0700

    Update ProtectedRoutes.jsx

--- frontend/src/Utils/ProtectedRoutes.jsx
+++ frontend/src/Utils/ProtectedRoutes.jsx
@@ -1,26 +1,3 @@
-// import { useSelector } from 'react-redux';
-// import { Navigate, Outlet, useLocation } from 'react-router-dom';
-// import PropTypes from "prop-types";
-
-// function ProtectedRoute({ children }) {
-//     const { isAuthenticated } = useSelector(state => state.auth);
-//     const location = useLocation();
-
-//     if (!isAuthenticated) {
-//         // Redirect to login page if not authenticated, preserving the intended location
-//         return <Navigate to="/login" state={{ from: location }} replace />;
-//     }
-
-//     // Render the children (the protected component) if authenticated
-//     return children;
-// }
-
-// ProtectedRoute.propTypes = {
-//     children: PropTypes.node.isRequired,
-// };
-
-// export default ProtectedRoute;
-
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
@@ -29,7 +6,7 @@ import { checkAuth } from '../apis/auth.js';
 const ProtectedRoute = () => {
     const location = useLocation();
     const [isAuthenticated, setIsAuthenticated] = useState(null);
-    
+
     useEffect(() => {
         const authStatus = async () => {
             const response = await checkAuth();
