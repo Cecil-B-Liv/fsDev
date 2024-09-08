51a594a92682c7346d786c23a4a59e30ff8a28f0
Frank-Vu <s3963172@rmit.edu.vn>
2024-09-08 12:37:13 +0700

    Update Redux location

--- frontend/src/redux/feature/authSlice.js
+++ frontend/src/redux/feature/authSlice.js
@@ -3,20 +3,26 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
     user: null,
     isAuthenticated: false,
-    
+
 };
 
 export const authSlice = createSlice({
-    name: 'auth', // Name of the slice
+    name: 'auth',
     initialState,
     reducers: {
         setUser: (state, action) => {
             state.user = action.payload;
             state.isAuthenticated = true;
+            localStorage.setItem('user', JSON.stringify(action.payload));   // Store user data
+            console.log('User set:', action.payload);   // For testing
+            localStorage.setItem('isAuthenticated', 'true'); // Store authentication status
+            console.log('Authentication status set:', state.isAuthenticated);   // For testing
         },
         logout: (state) => {
             state.user = null;
             state.isAuthenticated = false;
+            localStorage.removeItem('user'); // Remove user data
+            localStorage.removeItem('isAuthenticated'); // Remove authentication status
         },
     },
 });
