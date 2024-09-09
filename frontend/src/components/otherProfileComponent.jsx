76b24ecc7168995cc82febde1d5fba571521a4a2
Liv_ Parker <thanhcua04@gmail.com>
2024-09-09 11:12:43 +0700

    Update otherProfileComponent.jsx

--- frontend/src/components/otherProfileComponent.jsx
+++ frontend/src/components/otherProfileComponent.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { Container, Card, Row, Col, Button, Form } from "react-bootstrap";
 import { useParams } from 'react-router-dom';  // Import useParams
+import { checkAuth } from "../apis/auth.js";
 
-import { getUser } from "../apis/users";
+import { getUser, sendFriendRequest, removeFriend ,getUserFriends} from "../apis/users";
 import { getUserPosts } from "../apis/posts";
 
 
@@ -12,23 +13,32 @@ const OtherProfileComponent = () => {
   const assets = import.meta.env.VITE_SERVER_ASSETS;
 
   const [isLoading, setIsLoading] = useState(true);
+  const [isFriend, setIsFriend] = useState(null);
   const [error, setError] = useState(null);
   const [profile, setProfile] = useState({});
   const [posts, setPosts] = useState([]); // Initialized as an empty array
+  const [currentUserId, setCurrentUserId] = useState(""); // Initialize with null
+  const [userFriends, setUserFriends] = useState([]);
 
-  
-
-  // Get userID from url
+  // Get otherUserID from url
   const { userId } = useParams();  // Get userID from URL params
 
-  // GET USER DATA
+  const handleAddFriendRequest = () => {
+    // sendFriendRequest();
+  };
+
+  const handleUnfriendRequest = () => {
+    // removeFriend();
+  };
+
+  // GET OTHER USER DATA
   useEffect(() => {
     const fetchUserProfile = async () => {
       try {
         const response = await getUser(userId);
         setProfile(response);
-        setTempProfile(response);
-        console.log(response);
+        // setTempProfile(response);
+        // console.log(response);
       } catch (error) {
         console.error("Error fetching user profile:", error);
         setError(error);
@@ -40,13 +50,12 @@ const OtherProfileComponent = () => {
     fetchUserProfile();
   }, [userId]);
 
-  // GET USER POST
+  // GET OTHER USER POST
   useEffect(() => {
     const fetchUserPost = async () => {
       try {
         const response = await getUserPosts(userId);
         setPosts(response);
-        console.log(response);
       } catch (error) {
         console.error("Error fetching user posts:", error);
         setError(error);
@@ -57,6 +66,42 @@ const OtherProfileComponent = () => {
 
     fetchUserPost();
   }, [profile]);
+
+  // Fetch current authenticated user
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await checkAuth();
+        setCurrentUserId(response.userId);  // Set the user after checking authentication
+      } catch (error) {
+        console.error("Error fetching current user:", error);
+      }
+    };
+    fetchCurrentUser();
+  }, []);
+
+  // check friend relation
+  useEffect(() => {
+        const handleCheckFriend = async () => {
+      try {
+        // Fetch other user's friends
+        const friends = await getUserFriends(userId); 
+    
+        // Check if the current user is in the other user's friends list
+        const isFriend = friends.some(friend => friend._id.toString() === currentUserId);
+    
+        if (isFriend) {
+          setIsFriend(true);  // Set state to true if they are friends
+        } else {
+          setIsFriend(false); // Set state to false if they are not friends
+        }
+    
+      } catch (error) {
+        console.error('Error checking friend status:', error);
+      }
+    };
+    handleCheckFriend()
+  },[currentUserId])
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -82,7 +127,31 @@ const OtherProfileComponent = () => {
                   <p className="mb-1">Email: {profile.email}</p>
                   <p className="mb-0">Phone: {profile.telephone}</p>
             </Col>
+            
+            {/* Move the button to the right */}
+            {isFriend == null &&(    
+              <></>        
+            )}
+
+            {/* Move the button to the right */}
+            {!isFriend &&(            
+              <Col xs="auto" className="ms-auto">
+              <Button variant="primary" onClick={handleAddFriendRequest}>
+                Add Friend
+              </Button>
+            </Col>
+            )}
+
+            {isFriend &&(            
+              <Col xs="auto" className="ms-auto">
+              <Button variant="danger" onClick={handleUnfriendRequest}>
+                Unfriend
+              </Button>
+            </Col>
+            )}
+
           </Row>
+          
         </Card.Body>
       </Card>
       <>
